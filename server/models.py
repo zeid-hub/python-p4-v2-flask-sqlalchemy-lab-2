fb97994668d8c0690ce421fb659c2fd3f413a916
zeid <zeidhassan4@gmail.com>
2024-04-05 15:13:15 +0300

    Done

--- server/models.py
+++ server/models.py
@@ -11,22 +11,51 @@ metadata = MetaData(naming_convention={
 db = SQLAlchemy(metadata=metadata)
 
 
-class Customer(db.Model):
+class Customer(db.Model, SerializerMixin):
     __tablename__ = 'customers'
+
+    serialize_rules = ("-reviews.customer",)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
+
+    #Applying the relationships
+    reviews = db.relationship("Review", back_populates= "customer")
+
+    items = association_proxy('reviews', 'item')
 
     def __repr__(self):
         return f'<Customer {self.id}, {self.name}>'
 
 
-class Item(db.Model):
+class Item(db.Model, SerializerMixin):
     __tablename__ = 'items'
+
+    serialize_rules = ("-reviews.item",)
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     price = db.Column(db.Float)
 
+    reviews = db.relationship("Review", back_populates= "item")
+
     def __repr__(self):
         return f'<Item {self.id}, {self.name}, {self.price}>'
+
+class Review(db.Model, SerializerMixin):
+
+    __tablename__ = "reviews"
+
+    serialize_rules = ("-customer.reviews", "-item.reviews",)
+
+    id = db.Column(db.Integer(), primary_key=True)
+    comment = db.Column(db.String())
+    customer_id = db.Column(db.Integer(), db.ForeignKey('customers.id'))
+    item_id = db.Column(db.Integer(), db.ForeignKey('items.id'))
+
+    #Applying relationships between my classes
+    customer = db.relationship("Customer", back_populates= "reviews")
+    item = db.relationship("Item", back_populates= "reviews")
+
+    def __repr__(self):
+        return f'<Item {self.id}, {self.comment}, {self.customer_id}, {self.item_id}>'
